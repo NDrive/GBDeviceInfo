806f6aa54eb17259edcc860081f6dfb24b74731a
Vitor Magalhães <vitor.magalhaes@ndrive.com>
2018-07-31 10:19:30 +0100

    added share scheme for iOS

---
+++ GBDeviceInfo-iOS-Framework/GBDeviceInfo_iOS_Framework.h
@@ -0,0 +1,19 @@
+//
+//  GBDeviceInfo_iOS_Framework.h
+//  GBDeviceInfo-iOS-Framework
+//
+//  Created by Vitor Magalhães on 31/07/2018.
+//  Copyright © 2018 Luka Mirosevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GBDeviceInfo_iOS_Framework.
+FOUNDATION_EXPORT double GBDeviceInfo_iOS_FrameworkVersionNumber;
+
+//! Project version string for GBDeviceInfo_iOS_Framework.
+FOUNDATION_EXPORT const unsigned char GBDeviceInfo_iOS_FrameworkVersionString[];
+
+#import <GBDeviceInfo_iOS_Framework/GBDeviceInfo.h>
+
+
